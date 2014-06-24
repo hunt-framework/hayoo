65fec41693bc19220c6c1ea188f268fc1ec2d37b
Uwe Schmidt <uwe@home>
2014-06-24 19:55:54 +0200

    depricated client interface functions removed

--- hayooLib/src/Hayoo/Url.hs
+++ hayooLib/src/Hayoo/Url.hs
@@ -1,44 +1,57 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Hayoo.Url 
-(
+module Hayoo.Url
+    (
       queryForDocument
     , hayooQueryUrl
     , hayooUrl
     , urlForDocument
     , hackageSource
-)
+    )
 where
 
 
 import           Data.String.Conversions (cs, (<>))
-import           Data.Text (Text)
-import qualified Hunt.ClientInterface as H (Query, printQuery,  qOrs, qAnds, withBoost, qContext)
-import           Network.HTTP.Types (renderQuery, simpleQueryToQuery)
+import           Data.Text               (Text)
+
+import qualified Hunt.ClientInterface    as H (Query, printQuery, qAnds,
+                                               qContext, qOrs, setBoost)
+
+import           Network.HTTP.Types      (renderQuery, simpleQueryToQuery)
+
 
 -- | generates a rendered 'Query' by a @package@, a @module@ and a @function or type@.
+
 queryForDocument :: Text -> Text -> Text -> H.Query
-queryForDocument p m f = H.qOrs [H.withBoost 100 q1, H.withBoost 10 q2, q3]
+queryForDocument p m f
+    = H.qOrs [H.setBoost 100 q1, H.setBoost 10 q2, q3]
     where
-    q1 = H.qAnds ["name" `H.qContext` f, "package" `H.qContext` p, "module" `H.qContext` m]
-    q2 = H.qAnds ["name" `H.qContext` f, "package" `H.qContext` p]
-    q3 = H.qAnds ["name" `H.qContext` f]
+      q1 = H.qAnds ["name" `H.qContext` f, "package" `H.qContext` p, "module" `H.qContext` m]
+      q2 = H.qAnds ["name" `H.qContext` f, "package" `H.qContext` p]
+      q3 = H.qAnds ["name" `H.qContext` f]
+
 
 -- | gernerate a url to a @page@ and a 'H.Query'.
+
 hayooQueryUrl :: Int -> Text -> Text
 hayooQueryUrl 0 q = hayooUrl [("query", q)]
 hayooQueryUrl p q = hayooUrl [("query", q), ("page", cs $ show p)]
 
 -- | generte a url with a list or parameters
-hayooUrl :: [(Text, Text)] -> Text
-hayooUrl q = ("/") <> (cs $ renderQuery True $ simpleQueryToQuery q')
-    where
-    -- convert [(Text, Text)] -> [(ByteString, ByteString)]
-    q' = map (\(x, y) -> (cs x, cs y)) q
 
--- | gernerate a url to a 'Query' by a @package@, a @module@ and a @function or type@. 
+hayooUrl :: [(Text, Text)] -> Text
+hayooUrl q
+    = ("/") <> (cs $ renderQuery True $ simpleQueryToQuery q')
+    where
+      -- convert [(Text, Text)] -> [(ByteString, ByteString)]
+      q' = map (\(x, y) -> (cs x, cs y)) q
+
+-- | gernerate a url to a 'Query' by a @package@, a @module@ and a @function or type@.
+
 urlForDocument :: Text -> Text -> Text -> Text
-urlForDocument package mod func = hayooQueryUrl 0 $ H.printQuery $ queryForDocument package mod func
+urlForDocument package modul func
+    = hayooQueryUrl 0 $ H.printQuery $ queryForDocument package modul func
 
 hackageSource :: Text -> Text -> Text
-hackageSource p sub = "http://hackage.haskell.org/package/" <> p <> "/docs/" <> sub
+hackageSource p sub
+    = "http://hackage.haskell.org/package/" <> p <> "/docs/" <> sub
