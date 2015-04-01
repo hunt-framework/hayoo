eddf3895339ed76d5fd79aaab91cc0b39f8e144e
Alex Biehl <alexbiehl@gmail.com>
2015-04-02 01:07:24 +0200

    Stealing @UweSchmidt 's new packagerank impl

--- hayoo-hoogle-indexer/src/Hayoo/Index/PackageRank.hs
+++ hayoo-hoogle-indexer/src/Hayoo/Index/PackageRank.hs
@@ -7,26 +7,275 @@ module Hayoo.Index.PackageRank
     , DAG
     , Ranking
     , Score
-    , dagFromList
-    , dagToList
-    , ranking
+--    , dagFromList
+--    , dagToList
+--    , ranking
     , rankingStd
+    , d1
     )
 where
 
-import           Control.Arrow   ((>>>), second)
-import           Control.DeepSeq (NFData)
+import           Control.Applicative ( (<$>) )
+import           Control.Arrow       ((>>>), (***), second)
+import           Control.DeepSeq     (NFData)
 
+import           Data.Monoid
 import           Data.Map        (Map)
 import qualified Data.Map        as M
 import qualified Data.Map.Strict as SM
 import           Data.Maybe      (fromMaybe, fromJust)
+import qualified Data.List       as L
 import           Data.Set        (Set)
 import qualified Data.Set        as S
 
 -- {-
 import           Debug.Trace
 -- -}
+
+-- ------------------------------------------------------------
+-- {- new version of rank computation
+-- ------------------------------------------------------------
+
+newtype  Rel a = Rel (DAG a, DAG a)
+
+type     DAG a = Map a (Set a)
+
+type DAGList a = [(a, [a])]
+
+-- ----------------------------------------
+--
+-- Rel invariant and instances
+
+inv :: (Ord a) => Rel a -> Bool
+inv (Rel (s, p))
+  = (L.sort . dagToList $ s)
+    ==
+    (L.sort . map (\ (x, y) -> (y, x)) . dagToList $ p)
+
+instance Eq a => Eq (Rel a) where
+  (Rel r1) == (Rel r2) = fst r1 == fst r2
+
+instance Ord a => Monoid (Rel a) where
+  mempty
+    = empty
+
+  Rel (s1, p1) `mappend` Rel (s2, p2)
+    = Rel ( SM.unionWith S.union s1 s2
+          , SM.unionWith S.union p1 p2
+          )
+
+instance (Ord a, Show a) => Show (Rel a) where
+  show = show . toList
+
+-- ----------------------------------------
+--
+-- conversions from DAG or DAGList to list of edges
+
+dagToList :: DAG a -> [(a, a)]
+dagToList = dagListToList . map (second S.toList) . SM.toList
+
+dagListToList :: DAGList a -> [(a, a)]
+dagListToList dl = [(x, y) | (x, ys) <- dl, y <- ys]
+
+-- ----------------------------------------
+--
+-- Rel ops
+
+empty :: Rel a
+empty = Rel (SM.empty, SM.empty)
+
+singleton :: (a, a) -> Rel a
+singleton (x, y)
+  = Rel ( SM.singleton x (S.singleton y)
+        , SM.singleton y (S.singleton x)
+        )
+
+insert :: Ord a => (a, a) -> Rel a -> Rel a
+insert (x, y)  (Rel (s, p))
+  = Rel ( SM.insertWith S.union x (S.singleton y) s
+        , SM.insertWith S.union y (S.singleton x) p
+        )
+
+fromList :: Ord a => [(a, a)] -> Rel a
+fromList
+  = L.foldl' (flip insert) empty
+
+fromDAGList :: Ord a => DAGList a -> Rel a
+fromDAGList = fromList . dagListToList
+
+toList :: Rel a -> [(a, a)]
+toList = dagListToList . succToDAGList
+
+succToDAGList :: Rel a -> DAGList a
+succToDAGList = map (second S.toList) . SM.toList . succs
+
+predToDAGList :: Rel a -> DAGList a
+predToDAGList = map (second S.toList) . SM.toList . preds
+
+succs :: Rel a -> DAG a
+succs (Rel r) = fst r
+
+preds :: Rel a -> DAG a
+preds (Rel r) = snd r
+
+pr1 :: Rel a -> [a]
+pr1 = SM.keys . succs
+
+pr2 :: Rel a -> [a]
+pr2 = SM.keys . preds
+
+loops :: Ord a => Rel a -> [a]
+loops (Rel r)
+  = SM.keys . SM.filterWithKey S.member . fst $ r
+
+member :: Ord a => (a, a) -> Rel a -> Bool
+member (x, y) (Rel r)
+  = y `S.member` (fromMaybe S.empty $ SM.lookup x (fst r))
+
+-- --------------------
+--
+-- composition of binary relations, the core op for getting performance
+--
+-- optimization: the outer loop of the join
+-- is done over the map with the least size.
+--
+-- This is espessially effective if one of r1 or r2
+-- is a single element rel, as in filterCycles
+
+compose :: Ord a => Rel a -> Rel a -> Rel a
+compose r1@(Rel (s1, p1)) r2@(Rel (s2, p2))
+  | SM.size s2 <= SM.size p1 = composeR r1 r2
+  | otherwise              = composeL r1 r2
+
+composeR :: Ord a => Rel a -> Rel a -> Rel a
+composeR (Rel (s1, p1)) (Rel (s2, p2))
+  = SM.foldrWithKey' ins1 empty s2  -- outer loop over succ of rel2
+  where
+    ins1 y' zs r
+      = case SM.lookup y' p1 of
+         Nothing -> r
+         Just xs -> S.fold ins2 r zs  -- build the cart. prod over xs an zs
+                    where
+                      ins2 z r' = S.fold ins3 r' xs
+                        where
+                          ins3 x r'' = insert (x, z) r''
+
+composeL :: Ord a => Rel a -> Rel a -> Rel a
+composeL (Rel (s1, p1)) (Rel (s2, p2))
+  = SM.foldrWithKey' ins1 empty p1 -- outer loop over pred of rel1
+  where
+    ins1 y xs r
+      = case SM.lookup y s2 of
+         Nothing -> r
+         Just zs -> S.fold ins2 r zs  -- build the cart. prod over xs an zs
+                    where
+                      ins2 z r' = S.fold ins3 r' xs
+                        where
+                          ins3 x r'' = insert (x, z) r''
+
+infixr 9 <.>
+(<.>) :: Ord a => Rel a -> Rel a -> Rel a
+(<.>) = compose
+
+transClosure :: (Show a, Ord a) => Rel a -> Rel a
+transClosure r
+  | r' == r   = r
+  | otherwise = transClosure r'
+  where
+    r' = -- traceShow ("transClosure: r=" ++ show r) $
+         r <.> r
+
+-- ----------------------------------------
+
+-- | compute the none refexive closure
+-- of a relation given as list of pais
+--
+-- by removing all edges that would introduce a cycle
+-- (a pair (x,x)) in the transitive closure
+--
+-- Which edges are removed depends on the order
+-- in the input list.
+--
+-- This is a test for the filterCycles algorithm
+
+acyclicRelFromList :: (Ord a, Show a) => [(a, a)] -> Rel a
+acyclicRelFromList = snd . L.foldl' add1 (empty, empty)
+  where
+    add1 acc@(c, r) e@(x, y)
+      | x == y    = -- traceShow ("loop: " ++ show x ++ "->" ++ show x) $
+                    acc
+      | (x, x) `member` cs
+                  = -- traceShow ("cycle: " ++ show x ++ "->" ++ show y ++
+                    --            "->...->" ++ show x) $
+                    acc
+      | otherwise = -- traceShow ("rel=" ++ show r) $
+                    (cs, insert e r)
+      where
+        xy = singleton e
+        c1 = c  <.> xy  -- all x' connected with y via x
+        c2 = xy <.> c   -- all y' connected with x via y
+        c3 = c1 <.> c   -- c <.> xy <.> c: all x' connected with all y' via x and y
+        cs = c <> xy <> c1 <> c2 <> c3
+
+-- ----------------------------------------
+--
+-- very much the same as acyclicRelFromList
+-- but there's no Rel built
+
+filterCycles :: (Ord a, Show a) => [(a, a)] -> [(a, a)]
+filterCycles = fst . filterCycles'
+
+filterCycles' :: (Ord a, Show a) => [(a, a)] -> ([(a, a)], [(a,a)])
+filterCycles' = (reverse *** reverse) . snd . L.foldl' add1 (empty, ([], []))
+  where
+    add1 res@(r, (xs, ys)) e@(x, y)
+      | x == y    = -- traceShow ("loop: " ++ show x ++ "->" ++ show x) $
+                    (r, (xs, e : ys))
+      | (x, x) `member` rs
+                  = -- traceShow ("cycle: " ++ show x ++ "->" ++ show y ++
+                    --            "->...->" ++ show x) $
+                    (r, (xs, e : ys))
+      | otherwise = -- traceShow ("rel=" ++ show rs) $
+                    (rs, (e : xs, ys))
+      where
+        xy = singleton e
+        r1 = r  <.> xy  -- all x' connected with y via x
+        r2 = xy <.> r   -- all y' connected with x via y
+        r3 = r1 <.> r   -- r <.> xy <.> r: all x' connected with all y' via x and y
+        rs = r <> r3 <> r2 <> r1 <> xy
+
+-- ------------------------------------------------------------
+
+type Ranking a = Map a Score
+type Score     = Float
+
+ranking :: (Ord a, Show a) => Score -> Rel a -> Ranking a
+ranking w rel
+  = -- traceShow r $
+    ranks  -- dynamic programming: ranks is used within insertRank !!!
+  where
+    succs' = succs rel
+    preds' = preds rel
+    ranks  = foldl insertRank M.empty $ M.keys succs'
+
+    insertRank r' k
+      = M.insert k (w * (S.fold accRank (1/w) usedBy)) r'
+      where
+        usedBy           = fromMaybe S.empty . M.lookup k $ preds'
+        accRank k' acc'  = (fromJust . M.lookup k' $ ranks) + acc'
+
+rankingStd :: (Ord a, Show a) => DAGList a -> Ranking a
+rankingStd
+  -- use of strict map leads to complete evaluation of result
+  = SM.map scale . ranking deflate . acyclicRelFromList . dagListToList
+  where
+    scale    = (/10.0) . fromInteger . round . (*10) . (+1.0) . logBase 2
+    deflate  = 0.5
+
+-- -}
+
+-- ------------------------------------------------------------
+{- old and buggy version of rank computation
 -- ------------------------------------------------------------
 
 type DAGList a                  = [(a, [a])]
@@ -160,7 +409,7 @@ rankingStd                      = SM.map scale . ranking deflate . dagFromList
     where
       scale                     = (/10.0) . fromInteger . round . (*10) . (+1.0) . logBase 2
       deflate                   = 0.5
-
+-- -}
 -- ------------------------------------------------------------
 {- minimal test case
 
